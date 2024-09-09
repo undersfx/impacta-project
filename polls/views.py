aca2a80309950e33f200e777d1a7a1dab7c1fd83
undersfx <atchin_@hotmail.com>
2024-09-08 23:28:18 -0300

    refactor: use class based views for index, details and results

--- polls/views.py
+++ polls/views.py
@@ -2,19 +2,31 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
 
-def index(request):
-    latest_questions = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_questions}
-    return render(request, "polls/index.html", context=context)
+class IndexView(generic.ListView):
+    """Renders index template for the queryset result."""
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", context={"question": question})
+class DetailView(generic.DetailView):
+    """Renders details template for the question got from pk param."""
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    """Renders results template for the question got from pk param."""
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -28,8 +40,3 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
